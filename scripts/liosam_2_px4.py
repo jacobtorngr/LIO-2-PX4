c827b218b663bda9d56bda460c85bc281a8b67e8
Jacob Törngren <studio.jaa@gmail.com>
2023-07-21 10:14:14 +0200

    Fixed consistent timing

--- scripts/liosam_2_px4.py
+++ scripts/liosam_2_px4.py
@@ -15,42 +15,42 @@ def state_cb(msg: State):
     global current_state
     current_state = msg
 
-class OdometryTransformation:
-    def __init__(self):
-        self.odom_sub = rospy.Subscriber("odometry/imu", Odometry, callback = self.odometry_cb)
-        self.odom_pub = rospy.Publisher("mavros/odometry/out", Odometry, queue_size=10)
+# odometry_cb: Read LIO
+def odometry_cb(msg: Odometry):
+     global lio_msg
+     lio_msg = msg
 
-    def read_and_pub_lio_odom(self, msg: Odometry):
-            px4_compliant_msg = copy.deepcopy(msg)
+    
+
+class OdometryRelay:
+    def __init__(self):
+        self.odom_pub = rospy.Publisher("mavros/odometry/out", Odometry, queue_size=5)
+
+    def read_and_pub_lio(self, event=None):
+            global lio_msg
+            px4_compliant_msg = copy.deepcopy(lio_msg)
 
             # transform from FLU (ROS convention) to FRD (PX4 convention) 
             # according to https://docs.px4.io/main/en/ros/external_position_estimation.html
-            px4_compliant_msg.pose.pose.position.x = msg.pose.pose.position.y
-            px4_compliant_msg.pose.pose.position.y = msg.pose.pose.position.x
-            px4_compliant_msg.pose.pose.position.z = -msg.pose.pose.position.z
+            px4_compliant_msg.pose.pose.position.x = lio_msg.pose.pose.position.y
+            px4_compliant_msg.pose.pose.position.y = lio_msg.pose.pose.position.x
+            px4_compliant_msg.pose.pose.position.z = -lio_msg.pose.pose.position.z
 
-            px4_compliant_msg.pose.pose.orientation.x = msg.pose.pose.orientation.y
-            px4_compliant_msg.pose.pose.orientation.y = msg.pose.pose.orientation.x
-            px4_compliant_msg.pose.pose.orientation.z = -msg.pose.pose.orientation.z
+            px4_compliant_msg.pose.pose.orientation.x = lio_msg.pose.pose.orientation.y
+            px4_compliant_msg.pose.pose.orientation.y = lio_msg.pose.pose.orientation.x
+            px4_compliant_msg.pose.pose.orientation.z = -lio_msg.pose.pose.orientation.z
 
-            px4_compliant_msg.twist.twist.linear.x = msg.twist.twist.linear.x
-            px4_compliant_msg.twist.twist.linear.y = -msg.twist.twist.linear.y
-            px4_compliant_msg.twist.twist.linear.z = -msg.twist.twist.linear.z
+            px4_compliant_msg.twist.twist.linear.x = lio_msg.twist.twist.linear.x
+            px4_compliant_msg.twist.twist.linear.y = -lio_msg.twist.twist.linear.y
+            px4_compliant_msg.twist.twist.linear.z = -lio_msg.twist.twist.linear.z
 
-            px4_compliant_msg.twist.twist.angular.x = msg.twist.twist.angular.x
-            px4_compliant_msg.twist.twist.angular.y = -msg.twist.twist.angular.y
-            px4_compliant_msg.twist.twist.angular.z = -msg.twist.twist.angular.z
+            px4_compliant_msg.twist.twist.angular.x = lio_msg.twist.twist.angular.x
+            px4_compliant_msg.twist.twist.angular.y = -lio_msg.twist.twist.angular.y
+            px4_compliant_msg.twist.twist.angular.z = -lio_msg.twist.twist.angular.z
             
             px4_compliant_msg.child_frame_id = "base_link"
 
-    # odometry_cb: Read odometry
-    def odometry_cb(self, msg: Odometry):
-         self.read_and_pub_lio_odom(msg)
-         
-    
-
-
-    
+            self.odom_pub.publish(px4_compliant_msg)
         
 
 
@@ -63,13 +63,18 @@ if __name__ == "__main__":
 
     while(not rospy.is_shutdown() and not current_state.connected):
         rate.sleep()
-
     rospy.loginfo("Flight controller connected")
+    
     state_sub.unregister()
 
+    odom_sub = rospy.Subscriber("odometry/imu", Odometry, callback = odometry_cb)
 
-    odt = OdometryTransformation()
-    rospy.Timer(rospy.Duration(.02), odt.odometry_cb())
+    while "lio_msg" not in globals():
+         rate.sleep()
+    rospy.loginfo("Receiving LIO data")
+    
+    odr = OdometryRelay()
+    rospy.Timer(rospy.Duration(.02), odr.read_and_pub_lio)
 
     rospy.spin()
 
